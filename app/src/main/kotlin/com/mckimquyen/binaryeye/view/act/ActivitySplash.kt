274245f158c824db90283d60b6862650508f2ac9
roy <loitp@galaxyjoy.vn>
2025-05-03 13:50:14 +0700

    update

--- app/src/main/kotlin/com/mckimquyen/binaryeye/view/act/ActivitySplash.kt
+++ app/src/main/kotlin/com/mckimquyen/binaryeye/view/act/ActivitySplash.kt
@@ -2,10 +2,14 @@ package com.mckimquyen.binaryeye.view.act
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.mckimquyen.binaryeye.BaseActivity
+import com.mckimquyen.binaryeye.BuildConfig
 import com.mckimquyen.binaryeye.R
+import com.mckimquyen.binaryeye.sdkadbmob.AdMobManager
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class ActivitySplash : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,17 +19,39 @@ class ActivitySplash : BaseActivity() {
         // because that would happen after the app is fully
         // initialized what is too late.
 
-        setupViews()
+        lifecycleScope.launch {
+            var hasCalledGoToMain = false
+            val job = launch {
+                delay(3_000)
+                if (!hasCalledGoToMain) {
+                    hasCalledGoToMain = true
+                    Log.d("roy93~", "goToMain #1")
+                    goToMain()
+                }
+            }
+            AdMobManager.loadAppOpenAd(
+                context = this@ActivitySplash,
+                adUnitId = BuildConfig.ADMOB_APP_OPEN_ID,
+                onAdLoaded = {
+                    if (!hasCalledGoToMain) {
+                        hasCalledGoToMain = true
+                        job.cancel()
+                        Log.d("roy93~", "goToMain #2")
+                        goToMain()
+                        AdMobManager.showAppOpenAd(this@ActivitySplash)
+                    }
+                },
+            )
+        }
     }
 
-    fun setupViews() {
-        Handler(Looper.getMainLooper()).postDelayed(
-            {
-                startActivity(Intent(applicationContext, CameraActivity::class.java))
-                finish()
-            },
-            1000
-        )
+
+    private fun goToMain() {
+        return
+        val intent = Intent(this@ActivitySplash, CameraActivity::class.java)
+        startActivity(intent)
+        overridePendingTransition(0, 0)
+        finishAffinity()
     }
 }
 
