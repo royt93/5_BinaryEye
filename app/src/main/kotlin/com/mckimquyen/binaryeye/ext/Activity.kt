65de46a4e60b32eb543522e4c59df47d08dbe744
roy <loitp@galaxyjoy.vn>
2025-09-13 11:42:52 +0700

    Refactor in-app review logic and update app version
    - Updated `versionCode` to 202500913 and `versionName` to '2025.09.13' in `app/build.gradle`.
    - Removed `app/src/main/res/values-v35/styles.xml`.
    - Modified `rateAppInApp` function in `app/src/main/kotlin/com/mckimquyen/binaryeye/ext/Activity.kt`:
        - Wrapped the in-app review logic in a try-catch block to handle potential exceptions.

--- app/src/main/kotlin/com/mckimquyen/binaryeye/ext/Activity.kt
+++ app/src/main/kotlin/com/mckimquyen/binaryeye/ext/Activity.kt
@@ -15,7 +15,8 @@ import android.os.Build
 import android.provider.AlarmClock
 import android.provider.CalendarContract
 import android.util.Log
-import android.view.*
+import android.view.View
+import android.view.WindowManager
 import com.google.android.play.core.review.ReviewException
 import com.google.android.play.core.review.ReviewInfo
 import com.google.android.play.core.review.ReviewManagerFactory
@@ -158,36 +159,40 @@ fun Activity.setSoftInputMode(
 //}
 
 fun Activity.rateAppInApp(forceRateInApp: Boolean = false) {
-    //import gradle app
+    try {
+        //import gradle app
 //    implementation("com.google.android.play:review:2.0.2")
 //    implementation("com.google.android.play:review-ktx:2.0.2")
 
-    val sharedPreferences = getSharedPreferences("app_preferences", Context.MODE_PRIVATE)
-    val lastReviewTime = sharedPreferences.getLong("last_review_time", 0L)
-    Log.d("roy93~", "~~~~~~~~~~~~~~~~~requestReview lastReviewTime $lastReviewTime")
-    val currentTime = Calendar.getInstance().timeInMillis
-    val daysSinceLastReview = (currentTime - lastReviewTime) / (1000 * 60 * 60 * 24)
-    Log.d("roy93~", "requestReview forceRateInApp $forceRateInApp")
-    Log.d("roy93~", "requestReview daysSinceLastReview $daysSinceLastReview")
-    if (daysSinceLastReview >= 7 || forceRateInApp) {
+        val sharedPreferences = getSharedPreferences("app_preferences", Context.MODE_PRIVATE)
+        val lastReviewTime = sharedPreferences.getLong("last_review_time", 0L)
+        Log.d("roy93~", "~~~~~~~~~~~~~~~~~requestReview lastReviewTime $lastReviewTime")
+        val currentTime = Calendar.getInstance().timeInMillis
+        val daysSinceLastReview = (currentTime - lastReviewTime) / (1000 * 60 * 60 * 24)
+        Log.d("roy93~", "requestReview forceRateInApp $forceRateInApp")
+        Log.d("roy93~", "requestReview daysSinceLastReview $daysSinceLastReview")
+        if (daysSinceLastReview >= 7 || forceRateInApp) {
 //    if (daysSinceLastReview >= 7) {
-        val reviewManager = ReviewManagerFactory.create(this)
-        val request = reviewManager.requestReviewFlow()
-        request.addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                val reviewInfo: ReviewInfo = task.result
-                reviewManager.launchReviewFlow(this, reviewInfo)
-                sharedPreferences.edit().putLong("last_review_time", currentTime).apply()
-                Log.d("roy93~", "requestReview result ${task.result}")
-                Log.d("roy93~", "requestReview isSuccessful ${task.isSuccessful}")
-                Log.d("roy93~", "requestReview isCanceled ${task.isCanceled}")
-                Log.d("roy93~", "requestReview isComplete ${task.isComplete}")
-                Log.d("roy93~", "requestReview exception ${task.exception}")
-            } else {
-                @ReviewErrorCode val reviewErrorCode = (task.exception as ReviewException).errorCode
-                Log.e("roy93~", "requestReview error $reviewErrorCode")
+            val reviewManager = ReviewManagerFactory.create(this)
+            val request = reviewManager.requestReviewFlow()
+            request.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val reviewInfo: ReviewInfo = task.result
+                    reviewManager.launchReviewFlow(this, reviewInfo)
+                    sharedPreferences.edit().putLong("last_review_time", currentTime).apply()
+                    Log.d("roy93~", "requestReview result ${task.result}")
+                    Log.d("roy93~", "requestReview isSuccessful ${task.isSuccessful}")
+                    Log.d("roy93~", "requestReview isCanceled ${task.isCanceled}")
+                    Log.d("roy93~", "requestReview isComplete ${task.isComplete}")
+                    Log.d("roy93~", "requestReview exception ${task.exception}")
+                } else {
+                    @ReviewErrorCode val reviewErrorCode = (task.exception as ReviewException).errorCode
+                    Log.e("roy93~", "requestReview error $reviewErrorCode")
+                }
             }
         }
+    } catch (_: Exception) {
+        //do nothing
     }
 }
 
